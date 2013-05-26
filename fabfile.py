27034193da48af4c219bc6a9021ddc23fbc671d7
Tudor Golubenco <tudor.g@gmail.com>
2013-05-26 12:57:58 +0200

    Added support for vim
    My vimrc and a task to install pathogen plugins
    from a json spec. Cannot yet install pathogen itself.

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,11 @@
 from fabric.api import (
     local,
     task,
+    lcd,
+    env
 )
+import os
+import json
 
 
 @task
@@ -37,12 +41,54 @@ def save_git():
 
 
 @task
+def save_vim():
+    """
+    Saves vimrc configuration.
+    """
+    local("cp ~/.vimrc vim/vimrc")
+
+
+@task
+def deploy_vim():
+    """
+    Deploys vim configuration and the pathogen plugins.
+    """
+    local("cp vim/vimrc ~/.vimrc")
+    install_pathogen_plugins()
+
+
+@task
+def install_pathogen_plugins():
+    """
+    Downloads and installs the Pathogen modules as listed in
+    vim/pathogen_plugins.json
+    """
+    pathogen_plugins = json.load(open("vim/pathogen_plugins.json"))
+    for plugin in pathogen_plugins:
+        dir_ = os.path.expanduser("~/.vim/bundle/{0}".format(plugin["name"]))
+        if os.path.isdir(dir_):
+            print("Plugin {0} already installed".format(plugin["name"]))
+            continue
+
+        if not "git" in plugin:
+            print("Don't know how to install plugin {0}".format(plugin["name"]))
+            continue
+
+        with lcd("~/.vim/bundle"):
+            local("git clone {0}".format(plugin["git"]))
+
+        print("Plugin {0} installed".format(plugin["name"]))
+
+
+@task
 def deploy_all():
     deploy_fish()
     deploy_git()
+    deploy_vim()
 
 
 @task
 def save_all():
     save_fish()
     save_git()
+    save_vim()
