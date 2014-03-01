920b1afc7c57b9b1ef8bf7c8c7b25170c8af8007
Tudor Golubenco <tudor.g@gmail.com>
2014-03-01 11:50:00 +0100

    simplified the fish prompt functions, I don't need the color change handlers

--- fish/config.fish
+++ fish/config.fish
@@ -44,31 +44,6 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_prompt_normal (set_color normal)
   end
 
-  if not set -q -g __fish_custom_prompt_functions_defined
-    set -g __fish_custom_prompt_functions_defined
-
-    function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-      if status --is-interactive
-        set -e __fish_prompt_user
-        commandline -f repaint ^/dev/null
-      end
-    end
-
-    function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-      if status --is-interactive
-        set -e __fish_prompt_host
-        commandline -f repaint ^/dev/null
-      end
-    end
-
-    function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-      if status --is-interactive
-        set -e __fish_prompt_status
-        commandline -f repaint ^/dev/null
-      end
-    end
-  end
-
   set -l delim '>'
 
   switch $USER
@@ -99,14 +74,6 @@ function fish_prompt --description 'Write out the prompt'
     set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
   end
 
-  if not set -q __fish_prompt_user
-    set -g __fish_prompt_user (set_color $fish_color_user)
-  end
-  if not set -q __fish_prompt_host
-    set -g __fish_prompt_host (set_color $fish_color_host)
-  end
-
-  #echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) (__fish_hg_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 
   echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) (__fish_hg_prompt) (__fish_virtualenv_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 
